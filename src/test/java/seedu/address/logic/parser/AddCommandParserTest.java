4d6afff4d9c33e87618d430c646672fc8a7d81d3
huekoh <huexkoh@gmail.com>
2024-03-30 16:45:38 +0800

    Update test files

--- src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddCommandParserTest.java
@@ -223,7 +223,7 @@ public class AddCommandParserTest {
         // invalid tag
         assertParseFailure(parser, FIRSTNAME_DESC_BOB + LASTNAME_DESC_BOB + PHONE_DESC_BOB
             + SEX_DESC_BOB + PAYRATE_DESC_BOB + ADDRESS_DESC_BOB
-            + INVALID_TAG_DESC + VALID_TAG_COOK, Tag.MESSAGE_CONSTRAINTS);
+            + INVALID_TAG_DESC + TAG_DESC_COOK, Tag.MESSAGE_CONSTRAINTS);
 
         // two invalid values, only first invalid value reported
         assertParseFailure(parser, INVALID_FIRSTNAME_DESC + LASTNAME_DESC_BOB + PHONE_DESC_BOB
